0af3f22e0d8a6aeb5b5bfa50265b13092bd1b152
vkliufinskyi <vkliufinskyi@xtm-intl.com>
2026-05-26 16:41:50 +0300

    TR-03: added up to 3 cases

--- main.ts
+++ main.ts
@@ -2,7 +2,12 @@ function numberToString(number: number): string {
   if (number === 0) {
     return "zero";
   }
-
+  if (number === 1) {
+    return "one";
+  }
+  if (number === 2) {
+    return "two";
+  }
   return number.toString();
 }
 
